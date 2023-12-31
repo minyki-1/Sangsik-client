2087a142ef809e30fb2c93e389a2a83090b9a501
WooSng <wooseong.choi@unmanned-ai.com>
2023-12-31 22:51:01 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = (phase, { defaultConfig }) => {
+  if ('sassOptions' in defaultConfig) {
+    defaultConfig['sassOptions'] = {
+      includePaths: ['./src'],
+      prependData: `@import '~@styles/colors.scss';`,
+    }
+  }
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
 
-module.exports = nextConfig
+      return config;
+    },
+  };
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import './globals.scss'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body>{children}</body>
     </html>
   )
 }

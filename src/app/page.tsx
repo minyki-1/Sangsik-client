d9e463ccefbd68bda7975371507eeb1498222dcd
WooSng <cwstbp0426@gmail.com>
2024-01-02 01:31:09 +0900

    post 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Search from "@/components/Search";
+import Posts from "@/components/Posts";
 
 export default function Home() {
   return (
     <div>
       <Search />
+      <Posts />
     </div>
   )
 }
---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,14 @@
+import style from "./index.module.scss";
+
+export default function Posts() {
+  return (
+    <main className={style.postList}>
+      <div className={style.post}>
+        1
+      </div>
+      <div className={style.post}>
+        1
+      </div>
+    </main>
+  )
+}
